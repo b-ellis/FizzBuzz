d4b25d62b1798fff5d6898792382f4fd9a0f4ca8
b-ellis <bradjellis18@gmail.com>
2016-07-07 08:53:05 -0700

    final refactor

--- js/app.js
+++ js/app.js
@@ -3,6 +3,11 @@ $(document).ready(function(){
 	var input = prompt("Input a Value", "");
 	var number = +input;
 
+	while(isNaN(number) || (number %1 !=0)){
+		input = prompt("Please re-input the Value", "");
+		number = +input;
+	}
+
 	for(var i=1; i <= number; i++){ // 1 to number inclusive
 		if ((i % 3) === 0 && (i % 5) === 0) {
 		$('#list').append('fizzbuzz');
